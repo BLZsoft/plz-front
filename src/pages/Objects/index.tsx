f19843f114bb9481a666a4d9627e837622078541
Алексей Дудников <alexluffy@MacBook-Air-Aleksej.local>
2023-05-24 23:03:08 +0300

    Продолжаем работу с объектами

---
+++ src/pages/Objects/index.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'antd/es/button';
+import Paragraph from 'antd/es/typography/Paragraph';
+
+import { useApi } from '../../app/providers/with-api';
+
+import { partnersPaths } from './routes';
+
+const Objects = () => {
+  const navigate = useNavigate();
+
+  const api = useApi();
+
+  useEffect(() => {
+    api.partners.partnersControllerFindAll({ skip: 0, take: 10 }).then((d) => console.log(d));
+  }, []);
+
+  return (
+    <div>
+      <Paragraph>Список объектов</Paragraph>
+      <Button type="primary" onClick={() => navigate(partnersPaths.modify)} size="large">
+        Создать объект
+      </Button>
+    </div>
+  );
+};
+
+export default Objects;
