8150de5efa4f892aeacdc46b6ae1a135f0b6a9ea
Алексей Дудников <alexluffy@MacBook-Air-Aleksej.local>
2023-06-22 22:22:08 +0300

    Chat

---
+++ src/pages/Chat/ModifyObject/index.tsx
@@ -0,0 +1,13 @@
+import Button from 'antd/es/button';
+
+const ModifyObject = () => {
+  return (
+    <div>
+      <Button type="primary" size="large">
+        Сохранить
+      </Button>
+    </div>
+  );
+};
+
+export default ModifyObject;
---
+++ src/pages/Chat/index.tsx
@@ -0,0 +1,119 @@
+// @ts-nocheck
+import { useEffect, useState, useMemo } from 'react';
+
+// import { useNavigate } from 'react-router-dom';
+<DeleteTwoTone />;
+import { DeleteTwoTone, EditTwoTone, PlusOutlined } from '@ant-design/icons';
+import { Card, Space, Row, Col, Popover, FloatButton } from 'antd';
+import Paragraph from 'antd/es/typography/Paragraph';
+import { useApi } from 'shared/hooks/useApi';
+import { TooltipCustom } from 'shared/ui/TooltipCustom';
+// import Button from 'antd/es/button';
+
+// import { objectsPaths } from './routes';
+
+const floatingBtnStyle = {
+  right: '10%',
+  bottom: '10%',
+  width: '60px',
+  height: '60px',
+};
+const CardExtraComponent = ({ name, handleDelete }) => (
+  <Space direction="horizontal" size={10}>
+    <TooltipCustom key={'Редактировать'} placement="bottom" title={'Редактировать'}>
+      <EditTwoTone twoToneColor="#ff3f3d" />
+    </TooltipCustom>
+    <Popover title={`Удалить объект ${name} ?`} trigger="click" content={<a onClick={handleDelete}>Да</a>}>
+      <TooltipCustom key={'Удалить'} placement="bottom" title={'Удалить'}>
+        <DeleteTwoTone twoToneColor="#ff3f3d" />
+      </TooltipCustom>
+    </Popover>
+  </Space>
+);
+
+const Objects = () => {
+  // const navigate = useNavigate();
+  // TODO для демонстрации
+  const [objects, setObjectsList] = useState([]);
+  const [updateFlag, setUpdateFlag] = useState(true);
+
+  const api = useApi();
+
+  const deleteObject = (id: string) => () => {
+    api.objectsControllerRemove({ id }).then(() => {
+      setUpdateFlag(true);
+    });
+  };
+
+  useEffect(() => {
+    if (updateFlag === true) {
+      setUpdateFlag(false);
+    }
+  }, [updateFlag]);
+
+  const renderJournalList = useMemo(
+    () =>
+      objects?.length ? (
+        <Space direction="horizontal" size={16}>
+          {objects.map((obj: Record<string, string>) => (
+            <Card
+              key={obj?.id}
+              hoverable
+              title={obj?.name}
+              extra={<CardExtraComponent handleDelete={deleteObject(obj?.id)} name={obj?.name} />}
+              style={{ width: 400 }}
+            >
+              <Row>
+                <Col span={6}>
+                  <Paragraph strong>Адрес</Paragraph>
+                </Col>
+                <Col span={10} offset={8}>
+                  <Paragraph type="secondary">{obj?.objectAddress || 'Курск, Ленина 42'}</Paragraph>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <Paragraph strong>{obj?.sp2type}</Paragraph>
+                </Col>
+                <Col span={10} offset={8}>
+                  <Paragraph type="secondary">{obj?.sp2name}</Paragraph>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <Paragraph strong>Класс</Paragraph>
+                </Col>
+                <Col span={10} offset={8}>
+                  <Paragraph type="secondary">{obj?.class}</Paragraph>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6}>
+                  <Paragraph strong>Категория</Paragraph>
+                </Col>
+                <Col span={10} offset={8}>
+                  <Paragraph type="secondary">{obj?.category || '-'}</Paragraph>
+                </Col>
+              </Row>
+            </Card>
+          ))}
+        </Space>
+      ) : null,
+    [objects],
+  );
+
+  return (
+    <div>
+      {renderJournalList}
+      <FloatButton
+        tooltip={<div>Добавить объект</div>}
+        trigger="click"
+        icon={<PlusOutlined />}
+        type="primary"
+        style={floatingBtnStyle}
+      />
+    </div>
+  );
+};
+
+export default Objects;
