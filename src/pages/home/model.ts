af2f29281c6d440eb65bb4c2ba22a410676a2913
Danil Tankov <danil.tankoff@yandex.ru>
2023-07-10 21:19:53 +0700

    refactor(objects): небольшой рефакторинг

--- src/pages/home/model.ts
+++ src/pages/home/model.ts
@@ -1,31 +1,23 @@
-import { createEffect, createStore, sample } from 'effector';
+import { chainRoute } from 'atomic-router';
+import { undefined } from 'zod';
 
 import { chainAuthenticated } from '~/features/authn/protected-routes';
 
+import { objectsModel } from '~/entities/objects';
+
 import { routes } from '~/shared/lib/router';
-import { Database, getAuthnSupabase } from '~/shared/lib/supabase';
 
 export const currentRoute = routes.home;
 
 export const authenticatedRoute = chainAuthenticated(currentRoute);
 
-export type ObjectType = Database['public']['Tables']['objects']['Row'];
-
-const fetchDataFx = createEffect<void, ObjectType[]>(async () => {
-  const supabaseClient = await getAuthnSupabase();
-
-  const { data } = await supabaseClient.from('objects').select();
-
-  if (!data) {
-    throw new Error('data is empty');
-  }
-
-  return data;
+export const dataLoadedRoute = chainRoute({
+  route: authenticatedRoute,
+  beforeOpen: {
+    effect: objectsModel.fetchObjectsFx,
+    mapParams: () => undefined,
+  },
+  openOn: objectsModel.fetchObjectsFx.doneData,
 });
 
-export const $data = createStore<ObjectType[]>([]).on(fetchDataFx.doneData, (_, payload) => payload);
-
-sample({
-  clock: authenticatedRoute.opened,
-  target: fetchDataFx,
-});
+export const $data = objectsModel.$objects;
--- src/widgets/objectsTable/index.tsx
+++ src/widgets/objectsTable/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 import {
   ColumnDef,
@@ -10,33 +10,22 @@ import {
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
-} from "@tanstack/react-table"
+} from '@tanstack/react-table';
 
-import { Button } from "~/shared/ui/button"
-import { Input } from "~/shared/ui/input"
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/shared/ui/table"
+import { Button } from '~/shared/ui/button';
+import { Input } from '~/shared/ui/input';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '~/shared/ui/table';
 
 interface DataTableProps<TData, TValue> {
-  data: TData[]
-  columns: ColumnDef<TData, TValue>[]
-
+  data: TData[];
+  columns: ColumnDef<TData, TValue>[];
 }
 
-export function ObjectsTable<TData, TValue>({
-  columns,
-  data,
-}: DataTableProps<TData, TValue>) {
-  const [sorting, setSorting] = useState<SortingState>([])
-  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
-    []
-  )
+// FIXME: move TableRow to entities/objects/ui/table-row
+
+export function ObjectsTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
 
   const table = useReactTable({
     data,
@@ -51,82 +40,60 @@ export function ObjectsTable<TData, TValue>({
       sorting,
       columnFilters,
     },
-  })
+  });
 
   return (
-  <>
-    <div className="flex items-center py-4">
-      <Input
-        placeholder="Filter names..."
-        value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-        onChange={(event) =>
-          table.getColumn("name")?.setFilterValue(event.target.value)
-        }
-        className="max-w-sm"
-      />
-    </div>
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    <>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Filter names..."
+          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+          onChange={(event) => table.getColumn('name')?.setFilterValue(event.target.value)}
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-    <div className="flex items-center justify-end space-x-2 py-4">
-      <Button
-        variant="outline"
-        size="sm"
-        onClick={() => table.previousPage()}
-        disabled={!table.getCanPreviousPage()}
-      >
-        Previous
-      </Button>
-      <Button
-        variant="outline"
-        size="sm"
-        onClick={() => table.nextPage()}
-        disabled={!table.getCanNextPage()}
-      >
-        Next
-      </Button>
-    </div>
-  </>
-  )
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+          Previous
+        </Button>
+        <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+          Next
+        </Button>
+      </div>
+    </>
+  );
 }
